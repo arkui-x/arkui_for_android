627e16dc82da9dc737f55441154a63c17c6e1bff
quguiren <quguiren1@huawei-partners.com>
2025-11-20 14:56:47 +0800

    添加Android ace_trace接口实现
    Signed-off-by: quguiren <quguiren1@huawei-partners.com>

--- osal/ace_trace.cpp
+++ osal/ace_trace.cpp
@@ -15,41 +15,79 @@
 
 #include "base/log/ace_trace.h"
 
-#include <android/trace.h>
+#include "hitrace_meter.h"
 
-#include "base/utils/macros.h"
+#include "base/utils/utils.h"
 
 namespace OHOS::Ace {
+namespace {
+static constexpr uint64_t ACE_TRACE_COMMERCIAL = HITRACE_TAG_ACE | HITRACE_TAG_COMMERCIAL;
+static constexpr uint64_t ANIMATION_TRACE_COMMERCIAL = HITRACE_TAG_ANIMATION | HITRACE_TAG_COMMERCIAL;
+}
 
-void AceCountTrace(const char *key, int32_t count){}
-
-bool AceTraceEnabled()
+void AceCountTrace(const char *key, int32_t count)
 {
-    return ATrace_isEnabled();
+    CHECK_NULL_VOID(key);
+    std::string keyStr(key);
+    CountTrace(HITRACE_TAG_ACE, keyStr, count);
+}
+
+bool AceAsyncTraceEnable()
+{
+    return IsTagEnabled(HITRACE_TAG_ACE);
 }
 
 void AceTraceBegin(const char* name)
 {
-    if (name != nullptr) {
-        ATrace_beginSection(name);
-    } else {
-        ATrace_beginSection("");
-    }
+    CHECK_NULL_VOID(name);
+    std::string nameStr(name);
+    StartTrace(HITRACE_TAG_ACE, nameStr);
 }
 
 void AceTraceEnd()
 {
-    ATrace_endSection();
+    FinishTrace(HITRACE_TAG_ACE);
 }
 
-void AceAsyncTraceBegin(int32_t taskId, const char* name, bool isAnimationTrace) {}
+void AceAsyncTraceBegin(int32_t taskId, const char* name, bool isAnimationTrace)
+{
+    CHECK_NULL_VOID(name);
+    std::string nameStr(name);
+    uint64_t tag = isAnimationTrace ? HITRACE_TAG_ANIMATION : HITRACE_TAG_ACE;
+    StartAsyncTrace(tag, nameStr, taskId);
+}
 
-void AceAsyncTraceEnd(int32_t taskId, const char* name, bool isAnimationTrace) {}
+void AceAsyncTraceEnd(int32_t taskId, const char* name, bool isAnimationTrace)
+{
+    CHECK_NULL_VOID(name);
+    std::string nameStr(name);
+    uint64_t tag = isAnimationTrace ? HITRACE_TAG_ANIMATION : HITRACE_TAG_ACE;
+    FinishAsyncTrace(tag, nameStr, taskId);
+}
 
-void AceAsyncTraceBeginCommercial(int32_t taskId, const char* name, bool isAnimationTrace) {}
-void AceAsyncTraceEndCommercial(int32_t taskId, const char* name, bool isAnimationTrace) {}
+void AceAsyncTraceBeginCommercial(int32_t taskId, const char* name, bool isAnimationTrace)
+{
+    CHECK_NULL_VOID(name);
+    std::string nameStr(name);
+    uint64_t tag = isAnimationTrace ? ANIMATION_TRACE_COMMERCIAL : ACE_TRACE_COMMERCIAL;
+    StartAsyncTrace(tag, nameStr, taskId);
+}
 
-void AceTraceBeginCommercial(const char* name) {}
-void AceTraceEndCommercial() {}
+void AceAsyncTraceEndCommercial(int32_t taskId, const char* name, bool isAnimationTrace)
+{
+    CHECK_NULL_VOID(name);
+    std::string nameStr(name);
+    uint64_t tag = isAnimationTrace ? ANIMATION_TRACE_COMMERCIAL : ACE_TRACE_COMMERCIAL;
+    FinishAsyncTrace(tag, nameStr, taskId);
+}
 
+void AceTraceBeginCommercial(const char* name)
+{
+    StartTraceEx(HITRACE_LEVEL_COMMERCIAL, ACE_TRACE_COMMERCIAL, name);
+}
+
+void AceTraceEndCommercial()
+{
+    FinishTraceEx(HITRACE_LEVEL_COMMERCIAL, ACE_TRACE_COMMERCIAL);
+}
 } // namespace OHOS::Ace
