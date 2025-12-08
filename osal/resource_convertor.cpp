d10f0745dbf531483f0cfaea71b6a3c5f43edb8a
quguiren <quguiren1@huawei-partners.com>
2025-12-08 18:33:33 +0800

    Revert "安卓-资源解耦功能代码"
    This reverts commit 5a72993f3c5069fbdc3c7b4f3674bb5c32b55a18.

--- osal/resource_convertor.cpp
+++ osal/resource_convertor.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -99,7 +99,6 @@ Global::Resource::InputDevice ConvertInputDevice(bool deviceAccess)
 std::shared_ptr<Global::Resource::ResConfig> ConvertConfigToGlobal(const ResourceConfiguration& config)
 {
     std::shared_ptr<Global::Resource::ResConfig> newResCfg(Global::Resource::CreateResConfig());
-    CHECK_NULL_RETURN(newResCfg, nullptr);
     newResCfg->SetLocaleInfo(AceApplicationInfo::GetInstance().GetLanguage().c_str(),
         AceApplicationInfo::GetInstance().GetScript().c_str(),
         AceApplicationInfo::GetInstance().GetCountryOrRegion().c_str());
--- osal/system_properties.cpp
+++ osal/system_properties.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -244,7 +244,7 @@ bool SystemProperties::GetDebugPixelMapSaveEnabled()
 
 bool SystemProperties::GetResourceDecoupling()
 {
-    return true;
+    return false;
 }
 
 bool SystemProperties::IsPCMode()
