60b3167d856ae2aeb7631f1b41e1380059e77369
quguiren <quguiren1@huawei-partners.com>
2026-01-29 14:21:30 +0800

    add counter etc
    Signed-off-by: quguiren <quguiren1@huawei-partners.com>

--- osal/dynamic_module_helper.cpp
+++ osal/dynamic_module_helper.cpp
@@ -27,12 +27,21 @@ namespace {
 const std::string DYNAMIC_MODULE_LIB_PREFIX = "libarkui_";
 const std::string DYNAMIC_MODULE_LIB_POSTFIX = ".so";
 const std::unordered_map<std::string, std::string> soMap = {
+    {"Marquee", "marquee"},
+    {"Stepper", "stepper"},
+    {"StepperItem", "stepper"},
+    {"Slider", "slider"},
     {"Checkbox", "checkbox"},
     {"CheckboxGroup", "checkbox"},
     {"Gauge", "gauge"},
     {"Rating", "rating"},
-    {"FlowItem", "waterflow" },
-    {"WaterFlow", "waterflow" },
+    {"FlowItem", "waterflow"},
+    {"WaterFlow", "waterflow"},
+    {"Counter", "counter"},
+    {"Sidebar", "sidebar"},
+    {"ColumnSplit", "linearsplit"},
+    {"RowSplit", "linearsplit"},
+    {"Radio", "radio"},
 };
 } // namespace
 DynamicModuleHelper& DynamicModuleHelper::GetInstance()
